34409acd9de1f3d8b6cbd1ae45c5e5a2ab570272
katarem <crismigesp@gmail.com>
2026-03-23 13:28:02 +0000

    tests: booking cancellation feature tests for application layer

---
+++ src/main/java/io/gihtub/katarem/application/usecase/CancelBookingUseCaseImpl.java
@@ -0,0 +1,20 @@
+package io.gihtub.katarem.application.usecase;
+
+import io.gihtub.katarem.application.port.input.CancelBookingUseCase;
+import io.gihtub.katarem.application.port.output.BookingOutputPort;
+import io.gihtub.katarem.domain.model.Booking;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CancelBookingUseCaseImpl implements CancelBookingUseCase {
+
+    private final BookingOutputPort outputPort;
+    @Override
+    public Booking cancelBooking(UUID bookingId) {
+        throw new UnsupportedOperationException();
+    }
+}
